f8cc91d9745e9269eb8b7bbf2473c2249e33f0d1
KevinDo76 <quandohoang23@gmail.com>
2025-05-06 14:00:24 -0700

    ping test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, MessageActivityType } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
@@ -16,15 +16,22 @@ client.once(Events.ClientReady, readyClient => {
 // Log in to Discord with your client's token
 client.login(token);
 client.on("ready", () => {
+	console.log(client.application.id);
 	start()
 });
 
 
 function start()
 {
-	console.log("Logon")
-	const channel = client.channels.cache.get('1292683542509125703');
-	channel.send('hello!')
-  		.then(message => console.log(`Sent message: ${message.content}`))
-  		.catch(console.error);
+	let clientPingTag = "<@"+client.application.id+">";
+	client.get
+	client.on("messageCreate", async(message) => {
+		if(message.content.toLowerCase().startsWith(clientPingTag)) {
+		  let textContent = message.content.replace(clientPingTag, "");
+		  message.channel.send({ content: `${message.author}`+message.member.displayName+": "+textContent });
+		  console.log(textContent)
+		}
+	});
+
+	
 }
